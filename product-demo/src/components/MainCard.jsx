a0aa35ccf4ecf4777ef40daf6b5066a17b0a16d0
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-23 06:25:18 +0800

    structure table head

--- product-demo/src/components/MainCard.jsx
+++ product-demo/src/components/MainCard.jsx
@@ -1,11 +1,21 @@
-import * as React from 'react';
-import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper,Chip} from '@mui/material';
-function MainCard() {
+import * as React from 'react'
+import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper,Chip} from '@mui/material'
+
+function MainCard(cellphone) {
   return (
-    <div>
-      
-    </div>
+    <TableContainer className='pt-6 rounded-lg'>
+      <Table>
+        <TableHead className="bg-gray-100" font >
+          <TableRow>
+            <TableCell sx={{fontWeight:"bold"}} >Thumbnail</TableCell>
+            <TableCell sx={{fontWeight:"bold"}}>Name</TableCell>
+            <TableCell sx={{fontWeight:"bold"}}>Description</TableCell>
+            <TableCell sx={{fontWeight:"bold"}}>Price</TableCell>
+          </TableRow>
+        </TableHead>
+      </Table>
+    </TableContainer>
   );
 }
 
-export default MainCard;
+export default MainCard
