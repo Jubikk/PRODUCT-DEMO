6a9f9b4b1df0cfa727b05fabf18648e7f62f58d3
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-23 05:13:33 +0800

    added a card

--- product-demo/src/App.jsx
+++ product-demo/src/App.jsx
@@ -1,11 +1,18 @@
 import * as React from 'react';
 import Header from './components/Header.jsx'
+import SearchBar from './components/SearchBar.jsx';
+import { CardContent, Container } from '@mui/material';
 
 function App(props) {
   return (
-    <div>
-      <Header/>
-    </div>
+    <Container>
+      <Card>
+        <CardContent>
+        <Header/>
+        <SearchBar/>
+        </CardContent>
+      </Card>
+    </Container>
   );
 }
 
