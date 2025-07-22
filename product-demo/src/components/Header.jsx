be227c090148cc31a578cdf2eda440759a827b8d
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-23 04:31:51 +0800

    new header and a new file

--- product-demo/src/components/Header.jsx
+++ product-demo/src/components/Header.jsx
@@ -1,20 +1,12 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import CardContent from '@mui/material/CardContent';
+import React from 'react';
+import { Typography, Box } from '@mui/material';
 
-import { ThemeProvider } from '@mui/material/styles';
 
 function Header() {
   return (
-    <div flex justify-center items-center bg-primary text-white mb-4 MuiBox-root css-0>
-      <AppBar className="flex justify-center items-center bg-primary text-white mb-4 MuiBox-root css-0">
-      <h2 className="MuiTypography-root MuiTypography-h6 css-8xoudn">PRODUCT DEMO</h2>
-
-      </AppBar>
-    </div>
-    
+    <Box className="bg-primary text-black flex justify-center py-4 mb-4">
+      <Typography variant="h5">PRODUCTS DEMO</Typography>
+    </Box>
   );
 }
 
