091e018fca1072ca5d600ed36d9dfc847cf2315a
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-23 05:58:46 +0800

    import for table

--- product-demo/src/components/MainCard.jsx
+++ product-demo/src/components/MainCard.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-
+import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper,Chip} from '@mui/material';
 function MainCard() {
   return (
     <div>
