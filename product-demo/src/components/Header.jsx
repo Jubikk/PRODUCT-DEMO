5d95734f262c3edef04209110368ad4c406a9c45
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-23 04:21:28 +0800

    added a maincard

--- product-demo/src/components/Header.jsx
+++ product-demo/src/components/Header.jsx
@@ -2,16 +2,19 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-
+import CardContent from '@mui/material/CardContent';
 
 import { ThemeProvider } from '@mui/material/styles';
 
 function Header() {
   return (
-    <AppBar class="flex justify-center items-center bg-primary text-white mb-4 MuiBox-root css-0">
-      <h2>PRODUCT DEMO</h2>
+    <div flex justify-center items-center bg-primary text-white mb-4 MuiBox-root css-0>
+      <AppBar className="flex justify-center items-center bg-primary text-white mb-4 MuiBox-root css-0">
+      <h2 className="MuiTypography-root MuiTypography-h6 css-8xoudn">PRODUCT DEMO</h2>
 
-    </AppBar>
+      </AppBar>
+    </div>
+    
   );
 }
 
---
+++ product-demo/src/components/MainCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainCard(props) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default MainCard;
