61aec042e0ac92cd41539aa1111544f3946105dc
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-23 05:15:43 +0800

    fixed error not showing, forgot to import card

--- product-demo/src/App.jsx
+++ product-demo/src/App.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Header from './components/Header.jsx'
 import SearchBar from './components/SearchBar.jsx';
-import { CardContent, Container } from '@mui/material';
+import { Card,CardContent, Container } from '@mui/material';
 
 function App(props) {
   return (
