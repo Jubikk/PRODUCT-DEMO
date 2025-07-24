a1f77d3fc0744dd80eb62c77b4d3e3b0741137dc
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-24 08:26:56 +0800

    included "images: to fetch

--- product-demo/src/utils/products.js
+++ product-demo/src/utils/products.js
@@ -2,7 +2,7 @@
 
 export const fetchProducts = async () => {
   try {
-    const res = await fetch('https://dummyjson.com/products/category/smartphones?limit=100&select=title,description,thumbnail,id,price,discountPercentage,brand,category,stock,rating');
+    const res = await fetch('https://dummyjson.com/products/category/smartphones?limit=100&select=title,description,thumbnail,id,price,discountPercentage,brand,category,stock,rating,images');
     const data = await res.json();
     return data.products;
   } catch (error) {
