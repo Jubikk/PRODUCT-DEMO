14ae5ba908cedd0fde693f2c3c7aae1aaecd3787
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-30 04:27:45 +0800

    finished the functions for CRUD

--- product-demo/src/services/localProducts.js
+++ product-demo/src/services/localProducts.js
@@ -1,19 +1,29 @@
 
 
 const getProducts = () => {
-    
+  const products = localStorage.getItem('products');
+  return products ? JSON.parse(products) : [];
 }
 
-const addProduct = () => {
-    
+const addProduct = (product) => {
+  const products = getProducts();
+  products.push(product);
+  localStorage.setItem('products', JSON.stringify(products));
 } 
 
-const updateProduct = () => {
-    
+const updateProduct = (updateProduct ) => {
+  const products = getProducts();   
+  const index = products.findIndex(p => p.id === updateProduct.id); 
+  if (index !== -1) {
+    products[index] = updateProduct;
+    localStorage.setItem('products', JSON.stringify(products));
+  }
 }
 
-const deleteProduct = () => {
-    
+const deleteProduct = (id) => {
+  const products = getProducts();
+  const filteredProducts = products.filter(p => p.id !== id);
+  localStorage.setItem('products', JSON.stringify(filteredProducts));
 } 
 
 export default {
