6498374485136afd12bc0d81001577529f08e141
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-30 03:03:04 +0800

    added localProducts to save locally for CRUD

---
+++ product-demo/src/services/localProducts.js
@@ -0,0 +1,28 @@
+
+
+const getProducts = () => {
+    
+}
+
+const addProduct = () => {
+    
+} 
+
+const updateProduct = () => {
+    
+}
+
+const deleteProduct = () => {
+    
+} 
+
+export default {
+    getProducts,
+    addProduct,
+    updateProduct,
+    deleteProduct
+}
+
+
+
+
